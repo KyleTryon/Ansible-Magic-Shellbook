de619a552efd8bf7bd4f3c8e9e6ab57464bd2e09
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2022-09-26 14:50:29 -0400

    fix: dotfiles installer (#2)

--- install.sh
+++ install.sh
@@ -12,12 +12,19 @@ sudo apt install ansible -y
 # To keep the repo clean, I load my defaults from a separate untracked file.
 if [ -f ./defaults/.main.yml ]; then
   echo "Overwriting variables with secret defaults..."
-  mv ./defaults/.main.yml ./defaults/main.yml
+  cat ./defaults/.main.yml > ./defaults/main.yml
 fi
 ## In CI, I load from a JSON file loaded in the environment.
 if [ -f ./defaults/.main.json ]; then
   echo "Overwriting variables with secret defaults..."
-  mv ./defaults/.main.json ./defaults/main.yml
+  cat ./defaults/.main.json > ./defaults/main.yml
+fi
+
+# Ensure the default variables are set.
+if grep -q "John Doe" defaults/main.yml; then
+    echo "Please update the defaults/main.yml file with your own information."
+    echo "You may also provide a .main.yml file in the defaults directory."
+    exit 1
 fi
 
 
