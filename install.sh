fcbf917624d1ffcb8e4d952c7ccb21cc781f67f6
Kyle.Tryon <Kyle.Tryon@outlook.com>
2022-10-07 00:53:58 -0400

    nosudo

--- install.sh
+++ install.sh
@@ -27,20 +27,20 @@ echo '
 
 # Install and configure the default playbook on the local machine.
 echo "Updating the local machine..."
-sudo apt update -y
-sudo apt install software-properties-common -y
+apt update -y
+apt install software-properties-common -y
 
 # If Python 3 is not installed, install it.
 if ! command -v python3 &> /dev/null
 then
     echo "Installing Python 3 and pip..."
-    sudo apt install python3 python3-pip -y
+    apt install python3 python3-pip -y
 fi
 
 
 # Install Ansible.
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
 
 # To keep the repo clean, I load my defaults from a separate untracked file.
 if [ -f ./defaults/.main.yml ]; then
@@ -65,4 +65,4 @@ echo "Install Ansible Galaxy requirements"
 ansible-galaxy install -r requirements.yml
 
 echo "Running playbook"
-sudo ansible-playbook ./linux-dev.yml --extra-vars "@./defaults/main.yml"
+ansible-playbook ./linux-dev.yml --extra-vars "@./defaults/main.yml"
